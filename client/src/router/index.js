230c2fbb8d31625b5a724bb4b65d5f29fb49115d
Haalloobim <withbimatara@gmail.com>
2023-11-13 09:14:08 +0700

    feat: add main task vue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginPage from '../views/LoginPage.vue'
+import MainTask from '../views/MainTask.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/login', 
     name: 'login',
     component: LoginPage
+  },
+  {
+    path: '/main',
+    name: 'main', 
+    component: MainTask
   }
 ]
 
