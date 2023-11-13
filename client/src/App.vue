be071d0fde720726cfcb6e463ac33379d36f1ac3
Haalloobim <withbimatara@gmail.com>
2023-11-13 09:09:01 +0700

    test: Route Testing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="LandingPage container bg-slate-100 min-h-screen">
+    <RouterView/>
+  </div>
 </template>
 
 <style>
---
+++ client/src/views/LoginPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="landing">
+        <header class="h-screen bg-slate-100">
+            <div class="container h-full m-auto flex">
+                <div class="m-auto pt-12 pb-10 px-8 rounded-lg shadow-2xl bg-white">
+                    <h1 class="text-4xl font-crimson pb-3 px-3 item">Login Page</h1>
+                    <form>
+                        <div class="py-2 px-4 text-lg leading-6 space-y-4 text-gray-200">
+                            <div class="relative">
+                                <input autocomplete="off" id="name" name="name" type="text"
+                                    class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                    placeholder="Username" />
+                                <label for="name"
+                                    class="cursor-pointer absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
+                            </div>
+                            <div class="relative">
+                                <input autocomplete="off" id="password" name="password" type="password"
+                                    class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                    placeholder="Password" />
+                                <label for="password"
+                                    class="cursor-pointer absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                            </div>
+                            <div class="relative">
+                                <button onclick="confirm(event)"
+                                    class="mt-2 inline-block rounded bg-[#3B71CA] px-6 pb-2 pt-2.5 text-sm tracking-wider font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: 'LoginPage'
+}
+</script>
