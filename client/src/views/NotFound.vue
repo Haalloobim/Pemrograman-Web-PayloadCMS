c3ff05b878c24232af21a83d5626396e46d2e939
Haalloobim <withbimatara@gmail.com>
2023-11-14 17:01:40 +0700

    feat: add 404 page

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex min-h-screen justify-center items-center m-auto flex-col">
+      <h1>Not Found!</h1>
+      <p>
+        <a href="/">Go home?</a>
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  
+    export default {
+      name: 'NotFound'
+    }
+  
+  </script>
+  
+  <style scoped>
+  
+    center {
+      margin: 15vw;
+    }
+  
+    h1 {
+      color: var(--border);
+      font-size: 2em;
+    }
+  
+  </style>
