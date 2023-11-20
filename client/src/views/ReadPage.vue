6160d865421d9c32033574b52f1f4dc8a3b440b1
Haalloobim <withbimatara@gmail.com>
2023-11-20 20:55:31 +0700

    feat: change to component implement

--- client/src/views/ReadPage.vue
+++ client/src/views/ReadPage.vue
@@ -1,56 +1,14 @@
     <template>
-        <div class="flex flex-col items-center justify-center min-h-screen ">
-            <div class="flex flex-col items-center gap-y-2">
-                <h1 class="block mb-2 text-md font-medium text-gray-900">Data Anime</h1>
-                <div :id="'Result'" class="w-3/5"></div>
-                <table class="border border-collapse w-full anime-table">
-                    <tr class="bg-gray-200">
-                        <th class="p-2 text-center">Anime ID</th>
-                        <th class="p-2 text-center">Anime Title</th>
-                        <th class="p-2 text-center">Rating</th>
-                        <th class="p-2 text-center">Studio</th>
-                    </tr>
-                    <tr v-for="i in datas" :key="i.id">
-                        <td class="p-2 text-center">{{ i.id }}</td>
-                        <td class="p-2 text-center">{{ i.title }}</td>
-                        <td class="p-2 text-center">{{ i.rate }}</td>
-                        <td class="p-2 text-center">{{ i.studio }}</td>
-                    </tr>
-                </table>
-            </div>
-        </div>
+        <ReadTable></ReadTable>
     </template>
     
     <script>
+    import ReadTable from '@/components/ReadTable.vue';
+
 
     export default {
-        name: 'readPage',
-        data() {
-            return {
-                url: "http://localhost:13337/api/anime/", 
-                datas: [], 
-            };
-        },
-        mounted() {
-            this.loadData();
-        },
-        methods: {
-            async loadData() {
-                try {
-                    const response = await fetch(this.url, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    });
-                    const data = await response.json();
-                    console.log(data);
-                    this.datas = data.docs;
-                } 
-                catch (error) {
-                    console.error("Error adding data to the API:", error.message);
-                }
-            },
-        }
-    }
+    name: 'readPage',
+    
+    components: { ReadTable }
+}
     </script>
